65376511dd3f81b423696f7f384ac3a727bb7b61
Mike Fahmie <fahmiemike@gmail.com>
2023-07-26 09:26:36 -0400

    added token mapping to NpcForm.cs

--- WorldWeaver/NPCform.cs
+++ WorldWeaver/NPCform.cs
@@ -20,6 +20,29 @@ namespace WorldWeaver
         public NPCForm()
         {
             InitializeComponent();
+            PopulateTokenComboBox();
+        }
+
+        private void PopulateTokenComboBox()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString"].ConnectionString;
+            string selectQuery = "SELECT token_name, token_id FROM tokens";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand(selectQuery, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cmb_tokenSelect.Items.Add(new KeyValuePair<string, int>(reader["token_name"].ToString(), Convert.ToInt32(reader["token_id"])));
+                        }
+                    }
+                }
+            }
         }
 
 
@@ -55,7 +78,13 @@ namespace WorldWeaver
         }
         private void btnSaveNPC_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(txtbxSTR.Text))
+            if (cmb_tokenSelect.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a token for the NPC.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            else if (string.IsNullOrEmpty(txtbxSTR.Text))
             {
 
                 MessageBox.Show("Please fill out the Strength stat.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -137,6 +166,10 @@ namespace WorldWeaver
                 int HP = int.Parse(txtbxHP.Text);
                 int AC = int.Parse(textBxAC.Text);
 
+                // Get the selected token_id from the ComboBox
+                int selectedTokenId = ((KeyValuePair<string, int>)cmb_tokenSelect.SelectedItem).Value;
+
+
                 // Check if the text box is empty
                 if (string.IsNullOrEmpty(txtbxSTR.Text))
                 {
@@ -174,7 +207,7 @@ namespace WorldWeaver
                 {
                     connection.Open();
 
-                    using (SqlCommand command = new SqlCommand("INSERT INTO npc (name, race, strength, dexterity, constitution, intelligence, wisdom, charisma, hit_points, armor_class, character_notes, skills, abilities, attacks) VALUES (@name, @race, @strength, @dexterity, @constitution, @intelligence, @wisdom, @charisma, @hitPoints, @armorClass, @characterNotes, @skills, @abilities, @attacks)", connection))
+                    using (SqlCommand command = new SqlCommand("INSERT INTO npc (name, race, strength, dexterity, constitution, intelligence, wisdom, charisma, hit_points, armor_class, character_notes, skills, abilities, attacks, token_id) VALUES (@name, @race, @strength, @dexterity, @constitution, @intelligence, @wisdom, @charisma, @hitPoints, @armorClass, @characterNotes, @skills, @abilities, @attacks, @tokenId)", connection))
                     {
                         command.Parameters.AddWithValue("@name", name);
                         command.Parameters.AddWithValue("@race", race);
@@ -193,6 +226,8 @@ namespace WorldWeaver
                         command.Parameters.AddWithValue("@skills", skills);
                         command.Parameters.AddWithValue("@abilities", abilities);
                         command.Parameters.AddWithValue("@attacks", attacks);
+
+                        command.Parameters.AddWithValue("@tokenId", selectedTokenId);
 
                         command.ExecuteNonQuery();
                     }
@@ -300,7 +335,38 @@ namespace WorldWeaver
         }
         private void NPCForm_Load(object sender, EventArgs e)
         {
+            // Populate the cmb_tokenSelect combo box with token names from the database
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString"].ConnectionString;
+            string selectQuery = "SELECT token_id, token_name FROM tokens"; // Include token_id in the query
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand(selectQuery, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        // Create a list to hold KeyValuePair of token_name and token_id
+                        List<KeyValuePair<string, int>> tokenList = new List<KeyValuePair<string, int>>();
+
+                        while (reader.Read())
+                        {
+                            int tokenId = Convert.ToInt32(reader["token_id"]);
+                            string tokenName = reader["token_name"].ToString();
+                            // Add the token_name and token_id as a KeyValuePair to the list
+                            tokenList.Add(new KeyValuePair<string, int>(tokenName, tokenId));
+                        }
+
+                        // Bind the list of KeyValuePair to the ComboBox
+                        cmb_tokenSelect.DataSource = new BindingSource(tokenList, null);
+                        cmb_tokenSelect.DisplayMember = "Key"; // Display the token_name
+                        cmb_tokenSelect.ValueMember = "Value"; // Store the token_id as the value
+                    }
+                }
+            }
         }
+
         private void btnContinue_Click(object sender, EventArgs e)
         {
         }
